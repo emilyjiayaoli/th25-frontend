ce7d1e83f78aec135fe57efec50ea4f26171e651
Evelynn Chen <evelynnc@andrew.cmu.edu>
2025-02-07 20:56:16 -0500

    split up screen into 3 components

---
+++ app/Canvas.tsx
@@ -0,0 +1,7 @@
+export default function Canvas() {
+    return (
+      <div className="h-full bg-gray-200 p-4 flex items-center justify-center">
+        <p className="text-xl font-semibold">Canvas</p>
+      </div>
+    );
+  }
---
+++ app/Info.tsx
@@ -0,0 +1,7 @@
+export default function Info() {
+    return (
+      <div className="h-full bg-gray-300 p-4 flex items-center justify-center">
+        <p className="text-lg font-medium">Info</p>
+      </div>
+    );
+  }
